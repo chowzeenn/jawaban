3bf978680e5ffdab59e206ff1ccc971b77766afa
chowzeenn <55623124+chowzeenn@users.noreply.github.com>
2019-09-21 18:21:07 +0700

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,15 @@
+var biodata={
+    "nama":"eko mahdi siwi",
+    "age":24,
+    "address":"yogyakarta",
+    "hobbies":["fishing","gaming"],
+    "is_maried":false,
+    "list_school":["SMA Darul Hikmah",2006,2012,null],
+    "skill":["design"],
+    "interest_in_coding":true,
+
+
+
+};
+var myJSON =JSON.stringify(biodata);
+console.log(JSON.stringify(biodata));
